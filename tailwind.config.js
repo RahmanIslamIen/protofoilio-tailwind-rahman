9823624bbeefe5788afa06ff946f1b0b90ebfb3c
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-05-13 23:47:32 +0700

    ui profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+    },
   },
   plugins: [],
 };
